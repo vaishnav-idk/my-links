18dc1fef4e76831081130556196629a2eff20552
vaishnav-idk <63421367+vaishnav-idk@users.noreply.github.com>
2023-04-29 15:58:05 +0530

    Update index.js
    creating a transition effect

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 window.onload = function() {
-  const main = document.querySelector('main');
-  main.style.filter = 'blur(5px)';
-};
+    var main = document.querySelector('main');
+    main.style.filter = 'blur(5px)';
+    setTimeout(function() {
+        main.style.transition = 'filter 2s';
+        main.style.filter = 'blur(0)';
+    }, 2000);
+}
