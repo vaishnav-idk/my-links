4377a0c026c0da546fe947293773a6f6723cf738
vaishnav-idk <63421367+vaishnav-idk@users.noreply.github.com>
2023-06-10 16:15:54 +0530

    add testing link to title

--- index.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    var main = document.querySelector('main');
-    main.style.filter = 'blur(5px)';
-    setTimeout(function() {
-        main.style.transition = 'filter 2s';
-        main.style.filter = 'blur(0)';
-    }, 2000);
-}
